02b81f882bd1cc956722c64072cfe17aafac84c6
kplr-io <mehdi.lamrani@kplr.fr>
2023-03-06 15:41:51 +0100

    Update 2.tree_clients.py

--- exercices/02.tree/2.tree_clients.py
+++ exercices/02.tree/2.tree_clients.py
@@ -2,6 +2,7 @@
 import json
 from unidecode import unidecode
 from treelib import Tree
+import os
 
 # Get the directory path of the current Python file
 local_path = os.path.dirname(os.path.abspath(__file__))
